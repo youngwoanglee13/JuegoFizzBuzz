31164a9bc91d175068dcc7aacc5635e9bcd8115b
younglee <young.lee@ucb.edu.bo>
2023-03-20 23:37:41 -0400

    deberia devolver 1 2 al enviar 2

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,4 +1,5 @@
 function FizzBuzz(numero){
-    return "1";
+    if(numero==1) return "1";
+    if(numero==2) return "1 2";
 };
 export default FizzBuzz;
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -4,6 +4,9 @@ describe("FizzBuzz", () => {
     it("deberia devolver 1 al enviar 1", () => {
       expect(fizzbuzz(1)).toEqual("1");
     });
+    it("deberia devolver 1 2 al enviar 2", () => {
+        expect(fizzbuzz(2)).toEqual("1 2");
+      });
 
 
   });
