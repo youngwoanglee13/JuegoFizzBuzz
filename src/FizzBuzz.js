12af92b105a8fac0b987979515aec847a176d33a
younglee <young.lee@ucb.edu.bo>
2023-03-20 23:54:45 -0400

    deberia devolver FizzBuzz cuando es multiplo de 3 y 5

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,6 @@
 function FizzBuzz(numero){
     if(numero==1) return "1";
+    if(numero%3==0 && numero%5==0) return FizzBuzz(numero-1) +" FizzBuzz";
     if(numero%3==0) return FizzBuzz(numero-1) +" Fizz";
     if(numero%5==0) return FizzBuzz(numero-1) +" Buzz";
     return FizzBuzz(numero-1) +" "+ numero;
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -19,6 +19,9 @@ describe("FizzBuzz", () => {
     it("deberia devolver Buzz si es multiplo de 5", () => {
         expect(fizzbuzz(10)).toEqual("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz");
     });
+    it("deberia devolver FizzBuzz si es multiplo de 3 y 5", () => {
+        expect(fizzbuzz(15)).toEqual("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz");
+    });
 
 
 
